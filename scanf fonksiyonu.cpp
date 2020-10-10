b326fef6f57e6c73f537f98a7a8a0795fa88806b
Yunus Emre Usta <20060803@stu.omu.edu.tr>
2020-10-10 20:49:22 +0300

    Initial commit

---
+++ scanf fonksiyonu.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+int main() 
+{
+	setlocale(LC_ALL, "Turkish");
+	int sayi;
+	float kesirlisayi;
+	double kesirlisayı2;
+	char karakter;
+	char karakterdizisi[5];
+
+	printf("lütfen bir integer değer giriniz...\n");
+	scanf_s("%d", &sayi);
+	printf("lütfen bir float değer giriniz...\n");
+	scanf_s("%f", &kesirlisayi);
+	printf("lütfen bir double değer giriniz...\n");
+	scanf_s("%lf", &kesirlisayı2); 
+	printf("lütfen bir karakter değer giriniz...\n");
+	scanf_s(" %c", &karakter); // yazabilmek için bir boşlukbırak.
+	printf("lütfen 4 karakter değeri giriniz...\n");
+	scanf_s("%s",karakterdizisi,_countof(karakterdizisi));  
+
+
+
+	printf("Girdiğniz sayı: %d\n", sayi);
+	printf("Girdiğniz sayı: %f\n", kesirlisayi);
+	printf("Girdiğniz sayı: %lf\n", kesirlisayı2);
+	printf("Girdiğniz karakter: %c\n", karakter);
+	printf("Girdiğniz karakterdizisi: %s\n", karakterdizisi);
+	
+	return 0;
+}
