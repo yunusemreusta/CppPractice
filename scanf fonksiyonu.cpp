d34aab26c157cf7f2fa7ea13d61abc6dfd7e9e70
Yunus Emre Usta <20060803@stu.omu.edu.tr>
2020-10-10 21:05:18 +0300

    Değişkenler ingilizceye çevrildi

--- scanf fonksiyonu.cpp
+++ scanf fonksiyonu.cpp
@@ -1,33 +1,33 @@
 ﻿#include <stdio.h>
 #include <iostream>
 #include <locale.h>
+
 int main() 
 {
 	setlocale(LC_ALL, "Turkish");
-	int sayi;
-	float kesirlisayi;
-	double kesirlisayı2;
-	char karakter;
-	char karakterdizisi[5];
+
+	int number;
+	float floatNumber;
+	double doubleNumber;
+	char character;
+	char text[5];
 
 	printf("lütfen bir integer değer giriniz...\n");
-	scanf_s("%d", &sayi);
+	scanf_s("%d", &number);
 	printf("lütfen bir float değer giriniz...\n");
-	scanf_s("%f", &kesirlisayi);
+	scanf_s("%f", &floatNumber);
 	printf("lütfen bir double değer giriniz...\n");
-	scanf_s("%lf", &kesirlisayı2); 
+	scanf_s("%lf", &doubleNumber); 
 	printf("lütfen bir karakter değer giriniz...\n");
-	scanf_s(" %c", &karakter); // yazabilmek için bir boşlukbırak.
+	scanf_s(" %c", &character); // yazabilmek için bir boşlukbırak.
 	printf("lütfen 4 karakter değeri giriniz...\n");
-	scanf_s("%s",karakterdizisi,_countof(karakterdizisi));  
+	scanf_s("%s", text, _countof(text));  
 
-
-
-	printf("Girdiğniz sayı: %d\n", sayi);
-	printf("Girdiğniz sayı: %f\n", kesirlisayi);
-	printf("Girdiğniz sayı: %lf\n", kesirlisayı2);
-	printf("Girdiğniz karakter: %c\n", karakter);
-	printf("Girdiğniz karakterdizisi: %s\n", karakterdizisi);
+	printf("Girdiğniz sayı: %d\n", number);
+	printf("Girdiğniz sayı: %f\n", floatNumber);
+	printf("Girdiğniz sayı: %lf\n", doubleNumber);
+	printf("Girdiğniz karakter: %c\n", character);
+	printf("Girdiğniz karakterdizisi: %s\n", text);
 	
 	return 0;
 }
